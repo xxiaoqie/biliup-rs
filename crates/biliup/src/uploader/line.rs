0251e3a78b35b7c05aa71c211eb11170cc5acbad
企鹅君 <47202121+xxiaoqie@users.noreply.github.com>
2025-01-11 23:44:22 +0800

    fix title not found in this scope

--- crates/biliup/src/uploader/line.rs
+++ crates/biliup/src/uploader/line.rs
@@ -99,15 +99,16 @@ impl Parcel {
         if let Some(ref mut title) = video.title {
             let reg = Regex::new(r"[\u0600-\u06FF]").unwrap();
             *title = reg.replace_all(title, "").to_string();
-        }
-        // 限制长度，超出则添加 "..."
-        const MAX_LENGTH: usize = 80;
-        if title.chars().count() > MAX_LENGTH {
-            // 保留 MAX_LENGTH - 3 个字符，并在末尾添加 "..."
-            *title = title
-                .chars()
-                .take(MAX_LENGTH - 3)
-                .collect::<String>() + "...";
+
+            // 限制长度，超出则添加 "..."
+            const MAX_LENGTH: usize = 80;
+            if title.chars().count() > MAX_LENGTH {
+                // 保留 MAX_LENGTH - 3 个字符，并在末尾添加 "..."
+                *title = title
+                    .chars()
+                    .take(MAX_LENGTH - 3)
+                    .collect::<String>() + "...";
+            }
         }
         Ok(video)
     }
